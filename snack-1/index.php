3be0a7a3ba96ba373ce574600a2b06c3547de731
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-05-20 12:42:46 +0200

    struttara dati

---
+++ snack-1/index.php
@@ -0,0 +1,43 @@
+<?php
+/* 
+## Snack 1
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60
+*/
+
+$partite=[
+    [
+        'casa'=> 'Milano',
+        'ospite' => 'Roma',
+        'punti_casa' => 55,
+        'punti_ospite' => 30
+    ],
+    [
+        'casa'=> 'Bologna',
+        'ospite' => 'Bari',
+        'punti_casa' => 75,
+        'punti_ospite' => 70
+    ],
+    [
+        'casa'=> 'Virtus',
+        'ospite' => 'Belin',
+        'punti_casa' => 25,
+        'punti_ospite' => 50
+    ],
+    [
+        'casa'=> 'Albinoleffe',
+        'ospite' => 'Cagliari',
+        'punti_casa' => 45,
+        'punti_ospite' => 30
+    ],
+    [
+        'casa'=> 'Genova',
+        'ospite' => 'Francia',
+        'punti_casa' => 55,
+        'punti_ospite' => 54
+    ],
+];
+
+foreach($partite as $partita){
+    echo
+}
