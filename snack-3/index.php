809643d7776ee4dc6e312642cbf2cc8ef9c1f55a
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-05-23 16:01:47 +0200

    aggiunto css

--- snack-3/index.php
+++ snack-3/index.php
@@ -53,12 +53,6 @@ $dates = [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Post</title>
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
-    <style>
-        .card {
-            width: 33%;
-            padding: 1rem;
-        }
-    </style>
 </head>
 
 <body>
@@ -69,12 +63,14 @@ $dates = [
                 <div class="row">
                     <?php foreach ($value as $post) : ?>
                         <div class="col">
-                            <img src="https://picsum.photos/1200/600" alt="" class="img-fluid">
+                            <div class="card border">
+                                <img src="https://picsum.photos/1200/600" alt="" class="w-100">
 
-                            <div class="text">
-                                <small><?= $post['author']; ?></small>
-                                <h3><?= $post['title']; ?></h3>
-                                <p><?= $post['text']; ?></p>
+                                <div class="text">
+                                    <small><?= $post['author']; ?></small>
+                                    <h3><?= $post['title']; ?></h3>
+                                    <p><?= $post['text']; ?></p>
+                                </div>
                             </div>
                         </div>
                     <?php endforeach; ?>
